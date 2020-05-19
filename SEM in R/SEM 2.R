6da29af1d72fea0afcfe2b3a8a965be1304f5a39
Matthias Quinn <53357992+N3uralN3twork@users.noreply.github.com>
2020-05-19 12:37:19 -0400

    Variables

--- SEM in R/SEM 2.R
+++ SEM in R/SEM 2.R
@@ -20,14 +20,13 @@ summary(dataset)
 attach(dataset)
 
 #### Preprocessing ####
-dataset$MSA97 = as.character(dataset$MSA97)
+#dataset$MSA97 = as.character(datasetMSA97)
 dataset$gender <- as.character(dataset$gender)
 table(gender) #3,393 0's and 3,342 1's
 table(race)
 variables <- as.data.frame(names(dataset))
 
 #### Full Model ####
-
 FullModel <- 
 "
 #######Latent Variables
@@ -45,19 +44,20 @@ DELINQUENCY =~ juveniledestroyproperty + juvenilestealless
 CRIME =~ adultgun + adultdestroyproperty + adultstealless + adultstealmore + adultotherproperty
       + adultattack + adultselldrugs
 
-ELEMSCHOOL =~ TRAUMACRIME + TRAUMAPOVERTY + TRAUMAFAMILY + DELINQUENCY
+ELEMSCHOOL =~
      + elementmiddledropout 
      + elementarysuspend + middlesuspend
 
 HIGHSCHOOL =~ 
      + highdropout 
      + highsuspend
-     + SES
-     + citizenship
      
 #######Correlations
 
-#TRAUMACRIME ~~ TRAUMAPOVERTY 
+TRAUMACRIME ~~ TRAUMAPOVERTY 
+TRAUMACRIME ~~ TRAUMAFAMILY
+TRAUMAPOVERTY ~~ TRAUMAFAMILY
+DELINQUENCY ~~ CRIME
 
 #######Regressions
 
@@ -65,12 +65,14 @@ HIGHSCHOOL =~
 adultincarceration ~  TRAUMACRIME + TRAUMAPOVERTY + TRAUMAFAMILY + ELEMSCHOOL + HIGHSCHOOL + juvenileincarceration + CRIME
       + age +  twoparenthome + black + hispanic  + geography97 + SES + citizenship + gender
       
-HIGHSCHOOL ~ TRAUMACRIME + TRAUMAPOVERTY + TRAUMAFAMILY + ELEMSCHOOL + DELINQUENCY + juvenileincarceration + CRIME
+HIGHSCHOOL ~ TRAUMACRIME + TRAUMAPOVERTY + TRAUMAFAMILY + ELEMSCHOOL + DELINQUENCY + juvenileincarceration
       + age +  twoparenthome + black + hispanic  + geography97 + SES + citizenship + gender
-    
+   
 ELEMSCHOOL ~ TRAUMACRIME + TRAUMAPOVERTY + TRAUMAFAMILY + juvenileincarceration
-      + age + twoparenthome + black + hispanic + geography97 + citizenship + gender
+      + age + twoparenthome + black + hispanic + geography97 + SES + citizenship + gender
+
 "
+#### END FULL MODEL ####
 
 # There seems to be a problem with the elementary/middle school latent variable
 # Model won't converge if SES is included in the regression
@@ -79,21 +81,21 @@ ELEMSCHOOL ~ TRAUMACRIME + TRAUMAPOVERTY + TRAUMAFAMILY + juvenileincarceration
 fit1 <- cfa(FullModel, data=dataset, std.lv=TRUE)
 summary(fit1, standardized=TRUE, fit.measures=TRUE)
 
+
 # Checking the correlations
 corrs = lavInspect(fit1, what="cor.all")
 
 
 
-
-#### By Gender Model ####
-GenderModel <-
+#### Gender Model ####
+GenderModel <- 
 "
-#######Latent
+#######Latent Variables
 TRAUMACRIME =~ jail 
     + victim + gunshotless12 + gunshot12to18
 
 TRAUMAPOVERTY =~  unemployment + homeless 
-    + poverty1997 + poverty1998 + poverty 1999 + poverty2000 + poverty2001 + poverty2002
+    + poverty1997 + poverty1998 + poverty1999 + poverty2000 + poverty2001 + poverty2002
     
 TRAUMAFAMILY =~ death + hospitalization + divorce
 
@@ -103,37 +105,114 @@ DELINQUENCY =~ juveniledestroyproperty + juvenilestealless
 CRIME =~ adultgun + adultdestroyproperty + adultstealless + adultstealmore + adultotherproperty
       + adultattack + adultselldrugs
 
-ELEMSCHOOL  =~ TRAUMACRIME + TRAUMAPOVERTY + TRAUMAFAMILY + DELINQUENCY
+ELEMSCHOOL =~
      + elementmiddledropout 
      + elementarysuspend + middlesuspend
-     + SES 
-     + citizenship
-     
-HIGHSCHOOL  =~ 
+
+HIGHSCHOOL =~ 
      + highdropout 
-     + highsuspend  
-     + SES
-     + citizenship
+     + highsuspend
      
 #######Correlations
 
-#TRAUMACRIME ~~ TRAUMAPOVERTY 
+TRAUMACRIME ~~ TRAUMAPOVERTY 
+TRAUMACRIME ~~ TRAUMAFAMILY
+TRAUMAPOVERTY ~~ TRAUMAFAMILY
+DELINQUENCY ~~ CRIME
 
 #######Regressions
 
-adultincarceration ~  TRAUMACRIME + TRAUMAPOVERTY + TRAUMAFAMILY + ELEMSCHOOL + HIGHSCHOOL + CRIME + juvenileincarceration
-      +  age +  twoparenthome + black + hispanic  + geography97 + SES + citizenship
 
-HIGHSCHOOL ~  TRAUMACRIME + TRAUMAPOVERTY + TRAUMAFAMILY + ELEMSCHOOL  + juvenileincarceration
-      +  age +  twoparenthome + black + hispanic  + geography97 + SES + citizenship
-
-ELEMSCHOOL ~ TRAUMACRIME + TRAUMAPOVERTY + TRAUMAFAMILY + juvenileincarceration + geography97 + age + twoparenthome + black
+adultincarceration ~  TRAUMACRIME + TRAUMAPOVERTY + TRAUMAFAMILY + ELEMSCHOOL + HIGHSCHOOL + juvenileincarceration + CRIME
+      + age +  twoparenthome + black + hispanic  + geography97 + SES + citizenship
+      
+HIGHSCHOOL ~ TRAUMACRIME + TRAUMAPOVERTY + TRAUMAFAMILY + ELEMSCHOOL + DELINQUENCY + juvenileincarceration
+      + age +  twoparenthome + black + hispanic  + geography97 + SES + citizenship
+   
+ELEMSCHOOL ~ TRAUMACRIME + TRAUMAPOVERTY + TRAUMAFAMILY + juvenileincarceration
+      + age + twoparenthome + black + hispanic + geography97 + SES + citizenship
 "
+#### END Gender MODEL ####
 
 # There seems to be a problem with the elementary/middle school latent variable
-# Model won't converge if its included in the regressions
-fit2 <- cfa(GenderModel, data=dataset, std.lv=TRUE, group = "gender")
-summary(fit2, standardized=TRUE, fit.measures=TRUE)
+# Model won't converge if SES is included in the regression
+# Highest correlation in SES is 0.2
+
+fit1 <- cfa(GenderModel, data=dataset, std.lv=TRUE, group="gender")
+summary(fit1, standardized=TRUE, fit.measures=TRUE)
+
+
+
+#### Race*Gender MODEL####
+
+# 4 levels in the race variable
+# 2 levels in the gender variable
+# Total models = 4*2 = 8 models
+
+table(gender, race) # Num. obs. per pair
+
+
+#### Gender* Race MODEL####
+GenderRaceModel <- 
+  "
+#######Latent Variables
+TRAUMACRIME =~ jail 
+    + victim + gunshotless12 + gunshot12to18
+
+TRAUMAPOVERTY =~  unemployment + homeless 
+    + poverty1997 + poverty1998 + poverty1999 + poverty2000 + poverty2001 + poverty2002
+    
+TRAUMAFAMILY =~ death + hospitalization + divorce
+
+DELINQUENCY =~ juveniledestroyproperty + juvenilestealless
+         + juvenilestealmore + juvenileotherproperty + juvenileattack + juvenileselldrugs
+         
+CRIME =~ adultgun + adultdestroyproperty + adultstealless + adultstealmore + adultotherproperty
+      + adultattack + adultselldrugs
+
+ELEMSCHOOL =~
+     + elementmiddledropout 
+     + elementarysuspend + middlesuspend
+
+HIGHSCHOOL =~ 
+     + highdropout 
+     + highsuspend
+     
+#######Correlations
+
+TRAUMACRIME ~~ TRAUMAPOVERTY 
+TRAUMACRIME ~~ TRAUMAFAMILY
+TRAUMAPOVERTY ~~ TRAUMAFAMILY
+DELINQUENCY ~~ CRIME
+
+#######Regressions
+
+
+adultincarceration ~  TRAUMACRIME + TRAUMAPOVERTY + TRAUMAFAMILY + ELEMSCHOOL + HIGHSCHOOL + CRIME + juvenileincarceration
+      + age + twoparenthome + geography97 + SES + citizenship
+      
+HIGHSCHOOL ~ TRAUMACRIME + TRAUMAPOVERTY + TRAUMAFAMILY + ELEMSCHOOL + DELINQUENCY + juvenileincarceration
+      + age + twoparenthome + geography97 + SES + citizenship
+   
+ELEMSCHOOL ~ TRAUMACRIME + TRAUMAPOVERTY + TRAUMAFAMILY + juvenileincarceration
+      + age + twoparenthome + geography97 + SES + citizenship
+"
+#### END Gender*Race MODEL ####
+
+
+fit1 <- cfa(GenderRaceModel, data=test, std.lv=TRUE)
+summary(fit1, standardized=TRUE, fit.measures=TRUE)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
