f5de3582d31a6784eb0aee5a87b2dc0dd116d8f9
Matthias Quinn <53357992+N3uralN3twork@users.noreply.github.com>
2020-06-10 22:38:22 -0400

    BLR

--- SEM in R/Testing.py
+++ SEM in R/Testing.py
@@ -10,6 +10,7 @@ os.chdir(abspath)
 import pandas as pd
 import numpy as np
 import statsmodels.api as sm
+import statsmodels.formula.api as smf
 
 dataset = pd.read_csv("Subset.csv", header=0)
 
@@ -23,30 +24,52 @@ dataset.info()
 #########################################################
 ###           4. Training Split                       ###
 #########################################################
-X = dataset.drop(["Aincarceration"], axis=1)
+X = dataset.drop(["Aincarceration", "GR"], axis=1)
 X = sm.add_constant(X) # Don't forget to include the intercept
 y = dataset["Aincarceration"]
 y.value_counts()
 
+formula = """Aincarceration ~ jail + victim + gunshotless12 + gunshot12to18 + unemployment + homeless +
+    poverty1997 + poverty1998 + poverty1999 + poverty2000 + poverty2001 + poverty2002 + 
+    death + hospitalization + divorce + juveniledestroyproperty + juvenilestealless +
+    juvenilestealmore + juvenileotherproperty + juvenileattack + juvenileselldrugs + 
+    adultgun + adultdestroyproperty + adultstealless + adultstealmore + adultotherproperty +
+    adultattack + adultselldrugs + elementarysuspend + middlesuspend + elementmiddledropout +
+    highsuspend + highdropout + Jincarceration + highgrade15 + gender + black + hispanic + age + 
+    twoparenthome + SES + citizenship + geography97"""
 #########################################################
 ###           5. Logistic Regression                  ###
 #########################################################
 
 full = sm.Logit(y, X).fit()
 
+fit = smf.glm(formula=formula,
+              data=dataset,
+              family=sm.families.Binomial()).fit()
+
+#########################################################
+###           6. Results                              ###
+#########################################################
 dir(full)
 print(full.summary())
+print(full.conf_int())
 
+dir(fit)
+print(fit.summary())
+print(fit.summary2())
 # Odds Ratios:
 print(np.exp(full.params))
+print(np.exp(fit.params))
 
 # Coefficient p-values:
 print(full.pvalues)
+print(fit.pvalues)
 
-results = pd.concat([np.exp(full.params), full.pvalues], axis=1)
-results.columns = ["OddsRatio", "p-value"]
-
+results = pd.concat([np.exp(full.params), full.pvalues, np.exp(full.conf_int())], axis=1)
+results.columns = ["OddsRatio", "p-value", "Lower", "Upper"]
 print(results)
 
-
+results = pd.concat([np.exp(fit.params), fit.pvalues, np.exp(fit.conf_int())], axis=1)
+results.columns = ["OddsRatio", "p-value", "Lower", "Upper"]
+print(results)
 
