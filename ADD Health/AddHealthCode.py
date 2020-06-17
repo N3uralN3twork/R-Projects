bf0ac2de05adc6c82c6e1ec90b979d9ff8b3f325
Matthias Quinn <53357992+N3uralN3twork@users.noreply.github.com>
2020-06-17 16:56:33 -0400

    Update AddHealthCode.py

--- ADD Health/AddHealthCode.py
+++ ADD Health/AddHealthCode.py
@@ -3,14 +3,19 @@ Add Health Data
 Author: Matt Quinn
 Date: 17 June 2020
 """
+
+"Import the necessary libraries and data:"
 import pandas as pd
 
 wave1 = pd.read_csv("C:/Users/miqui/Downloads/ICPSR_21600-V21/ICPSR_21600/DS0001/21600-0001-Data.tsv",
                     sep="\t", verbose=1)
-wave1.info()
-
 wave2 = pd.read_csv("C:/Users/miqui/Downloads/ICPSR_21600-Wave2/ICPSR_21600/DS0005/21600-0005-Data.tsv",
                     sep="\t")
+wave1.info()
 wave2.info()
 
 wave1["AID"].value_counts()
+wave2["AID"].value_counts()
+
+"Merge waves by AID:"
+
