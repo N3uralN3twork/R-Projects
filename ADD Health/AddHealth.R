9d8215a473f66c60ebaf7544a9043a8f82e27bbb
Matthias Quinn <53357992+N3uralN3twork@users.noreply.github.com>
2020-06-17 22:09:05 -0400

    ADD Health

--- ADD Health/AddHealth.R
+++ ADD Health/AddHealth.R
@@ -1,5 +1,6 @@
 "Read in the data and necessary libraries:"
 library(dplyr)
+library(lubridate)
 library(readr)
 wave1 <- read_delim("C:/Users/miqui/Downloads/ICPSR_21600-V21/ICPSR_21600/DS0001/21600-0001-Data.tsv", 
                     "\t", escape_double = FALSE, trim_ws = TRUE)
@@ -21,3 +22,21 @@ table(is.na(wave1$AID))
 "Merge waves by AID:"
 
 waves <- merge(wave1, wave2, by="AID")
+
+
+waves <- waves %>%
+          select(AID, IMONTH, IDAY, IYEAR, H1GI1M, H1GI1Y)
+
+"Calculating the Age:"
+
+waves <- waves %>% 
+  mutate(IDate = mdy(paste(IMONTH, IDAY, IYEAR)))
+
+waves <- waves %>%
+  mutate(BDate = mdy(paste(H1GI1M, 15, H1GI1Y)))
+
+waves <- waves %>%
+  mutate(Age = as.numeric((IDate - BDate)/365.25))
+
+summary(waves$Age)
+
--- ADD Health/AddHealthCode.py
+++ ADD Health/AddHealthCode.py
@@ -6,6 +6,7 @@ Date: 17 June 2020
 
 "Import the necessary libraries and data:"
 import pandas as pd
+import datetime
 
 wave1 = pd.read_csv("C:/Users/miqui/Downloads/ICPSR_21600-V21/ICPSR_21600/DS0001/21600-0001-Data.tsv",
                     sep="\t", verbose=1)
@@ -20,3 +21,9 @@ wave2["AID"].value_counts()
 "Merge waves by AID:"
 
 waves = wave1.merge(wave2, on=["AID"])
+
+month = "6"
+day = "23"
+year = "95"
+
+date = day + "-" + month + "-" + year
